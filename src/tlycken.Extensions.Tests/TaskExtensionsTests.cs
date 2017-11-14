fcf210f7f20875e61dfceafda08aa4b2bd47553c
Tomas Lycken <tlycken@users.noreply.github.com>
2017-11-14 10:39:34 +0100

    Add tests for Task<T> extensions

---
+++ src/tlycken.Extensions.Tests/TaskExtensionsTests.cs
@@ -0,0 +1,40 @@
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace tlycken.Extensions.Tests
+{
+    public class TaskExtensionsTests
+    {
+        [Fact]
+        public async Task CanAwaitIEnumerableAsList()
+        {
+            var task = Task.FromResult(new[] { 1, 2, 3, 4 }.AsEnumerable());
+
+            List<int> list = await task.ToList();
+
+            Assert.IsType<List<int>>(list);
+            Assert.Collection(list,
+                i => Assert.Equal(1, i),
+                i => Assert.Equal(2, i),
+                i => Assert.Equal(3, i),
+                i => Assert.Equal(4, i));
+        }
+
+        [Fact]
+        public async Task CanAwaitIEnumerableAsReadOnlyCollection()
+        {
+            var task = Task.FromResult(new[] { 1, 2, 3, 4 }.AsEnumerable());
+
+            IReadOnlyCollection<int> roc = await task.ToReadOnlyCollection();
+
+            Assert.IsAssignableFrom<IReadOnlyCollection<int>>(roc);
+            Assert.Collection(roc,
+                i => Assert.Equal(1, i),
+                i => Assert.Equal(2, i),
+                i => Assert.Equal(3, i),
+                i => Assert.Equal(4, i));
+        }
+    }
+}
