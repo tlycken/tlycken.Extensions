7a3750833d3598f90917229fe5da67ae6724d0ca
Tomas Lycken <tlycken@users.noreply.github.com>
2018-01-17 14:26:35 +0100

    Implement IEnumerable<T>.Indexed() (#5)

--- src/tlycken.Extensions.Tests/EnumerableExtensions.cs
+++ src/tlycken.Extensions.Tests/EnumerableExtensions.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Linq;
 using Xunit;
 
@@ -37,6 +38,23 @@ namespace tlycken.Extensions.Tests
             Assert.Equal(expected, actual);
         }
 
+        [Fact]
+        public void Indexed_OnEnumerable_ReturnsTupleWithValueInFirstPos()
+        {
+            var random = new Random();
+            var input = Enumerable.Range(0, 5).Select(i => random.Next()).ToList();
+
+            Assert.Equal(input, input.Indexed().Select(t => t.Item1));
+        }
+
+        [Fact]
+        public void Indexed_OnEnumerable_ReturnsTupleWithIndicesInSecondPos()
+        {
+            var random = new Random();
+            var input = Enumerable.Range(0, 5).Select(i => random.Next());
+
+            Assert.Equal(Enumerable.Range(0, 5), input.Indexed().Select(t => t.Item2));
+        }
 
         private class Foo
         {
