4a417e10a572ec3fd7014d34749e77e4cacc3e8e
Tomas Lycken <tlycken@users.noreply.github.com>
2018-01-25 14:01:54 +0100

    Whitespace fix

--- src/tlycken.Extensions/EnumerableExtensions.cs
+++ src/tlycken.Extensions/EnumerableExtensions.cs
@@ -13,6 +13,6 @@ namespace System.Linq
         public static IEnumerable<T> Distinct<T, TKey>(this IEnumerable<T> source, Func<T, TKey> keySelector)
             => source.GroupBy(keySelector).Select(group => group.First());
 
-        public static IEnumerable<(T,int)> Indexed<T>(this IEnumerable<T> source) => source.Select((x,i) => (value: x, index: i));
+        public static IEnumerable<(T, int)> Indexed<T>(this IEnumerable<T> source) => source.Select((x, i) => (value: x, index: i));
     }
 }
